244bb2c587e54c1c91fa4b9aa542bb763d6a084f
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2024-03-23 13:37:35 -0500

    fixing bugs in the layout exports

--- src/app/update/layout.tsx
+++ src/app/update/layout.tsx
@@ -1,6 +1,7 @@
 import { Toaster } from "@/components/ui/toaster";
+import type { Metadata } from "next";
 
-export const metaData = {
+export const metaData: Metadata = {
   title: "Update task",
 };
 export default function Layout({ children }: { children: React.ReactNode }) {
